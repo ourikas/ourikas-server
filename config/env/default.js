8f156fb5033b3c6109c228fec4ca0872f8305418
Mauricio Melo <mauricio@mauriciomelo.com.br>
2016-03-04 10:30:01 -0300

    Rename test directories to tests

--- config/env/default.js
+++ config/env/default.js
@@ -1,6 +1,6 @@
 module.exports = {
   assets: {
     routes: 'modules/**/routes/**/*js',
-    factories: 'modules/**/test/**/factories/**/*js',
+    factories: 'modules/**/tests/**/factories/**/*js',
   },
 };
